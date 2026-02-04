b466c95acf9d9df8606f052ce598a2ba6e792195
khanwaqas444 <waqaskhan4447770@gmail.com>
2026-02-04 21:23:24 +0530

    Hospital Management

---
+++ UserMS/src/main/java/com/hms/user/UserMS/dto/ResponseDTO.java
@@ -0,0 +1,8 @@
+package com.hms.user.UserMS.dto;
+
+import lombok.Data;
+
+@Data
+public class ResponseDTO {
+  String message;
+}
---
+++ UserMS/src/main/java/com/hms/user/UserMS/dto/Roles.java
@@ -0,0 +1,5 @@
+package com.hms.user.UserMS.dto;
+
+public class Roles {
+
+}
